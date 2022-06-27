d2215b0210fc7a3027dd44cc0e216fd9725ec1cf
Dev.Hardy <68279162+hazzang777@users.noreply.github.com>
2022-06-27 23:56:44 +0900

    sovled pro1330

---
+++ break-algorithm/src/main/java/me/devhardy/baekjoon/bronze/Pro1330.java
@@ -0,0 +1,19 @@
+package me.devhardy.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Pro1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a > b) {
+            System.out.println(">");
+        }else if (a < b){
+            System.out.println("<");
+        }else{
+            System.out.println("==");
+        }
+    }
+}
