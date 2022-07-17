205e286a1380698bf48b49fe8cc4f0f5b11ebccb
Dev.Hardy <68279162+hazzang777@users.noreply.github.com>
2022-07-18 00:44:27 +0900

    [solved] pro 10950

---
+++ break-algorithm/src/main/java/me/devhardy/baekjoon/bronze/Pro10950.java
@@ -0,0 +1,15 @@
+package me.devhardy.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Pro10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
