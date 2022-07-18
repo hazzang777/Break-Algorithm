83d0b38b432928380837e0970efbb72d627f4884
Dev.Hardy <68279162+hazzang777@users.noreply.github.com>
2022-07-18 23:11:25 +0900

    [solved] pro 11654

---
+++ break-algorithm/src/main/java/me/devhardy/baekjoon/bronze/Pro11654.java
@@ -0,0 +1,12 @@
+package me.devhardy.baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class Pro11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        sc.close();
+        System.out.println((int) c);
+    }
+}
